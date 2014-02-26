fcbe6c8e3dd82fd3076efc21343714408178245e
Dom Leone <ohheydom@gmail.com>
2014-02-26 14:29:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MetsFeed::Application.config.secret_key_base = '7d990bcd37358c3d0b8c9226e5923295078e454b9cf399a27acc4031f5addf86beccd1ab037b2dd7c0c5f33fdfb02f743d04372c3b33fba24d53e5da7f65f01b'
