d226b0e57b33f7b3b066930c1ea8bfd638a7608a
Dom Leone <ohheydom@gmail.com>
2014-03-06 10:05:02 -0500

    Fixed a few security issues with secret_token and outdated rails.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,12 @@
-# Be sure to restart your server when you modify this file.
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-MetsFeed::Application.config.secret_key_base = '7d990bcd37358c3d0b8c9226e5923295078e454b9cf399a27acc4031f5addf86beccd1ab037b2dd7c0c5f33fdfb02f743d04372c3b33fba24d53e5da7f65f01b'
+MetsFeed::Application.config.secret_key_base = secure_token
